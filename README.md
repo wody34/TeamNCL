32abd82563b147e277119e05b52fef0402dca784
wody34 <s.h_kim@kaist.ac.kr>
2016-08-20 03:32:28 +0900

    ChargingStation Model/Controller 추가

---
+++ README.md
@@ -0,0 +1,3 @@
+# TeamNCL
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/ChargingStationController.js
@@ -0,0 +1,11 @@
+/**
+ * ChargingStationController
+ *
+ * @description :: Server-side logic for managing Chargingstations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
