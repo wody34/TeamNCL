d7680280aa0adf409982019ab7142a74bc59ad26
wody34 <s.h_kim@kaist.ac.kr>
2016-08-20 03:49:43 +0900

    EV Charging Station Open API 규격에 따른 Model 생성

--- api/models/ChargingStation.js
+++ api/models/ChargingStation.js
@@ -8,7 +8,42 @@
 module.exports = {
 
   attributes: {
-
+    statId: {
+      type: 'string',
+      size: 20
+    },
+    statNm: {
+      type: 'string',
+      size: 100
+    },
+    chgerId: {
+      type: 'string',
+      size: 16
+    },
+    chgerType: {
+      type: 'string',
+      enum: ['01', '03', '06']
+    },
+    stat: {
+      type: 'string',
+      enum: ['1', '2', '3', '4', '5']
+    },
+    lat: {
+      type: 'string',
+      size: 20
+    },
+    lng: {
+      type: 'string',
+      size: 20
+    },
+    addr: {
+      type: 'string',
+      size: 150
+    },
+    useTime: {
+      type: 'string',
+      size: 50
+    }
   }
 };
 
