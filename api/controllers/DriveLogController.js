fc579b5e72959c0efa32f0cc569d44af4fffbe6f
SeongHwan Kim <s.h_kim@kaist.ac.kr>
2016-08-22 15:54:01 +0900

    차량 이동 로직을 모듈화하여 다중 차량 지원 차량 정보 및 이동 로그 기록

---
+++ api/controllers/DriveLogController.js
@@ -0,0 +1,11 @@
+/**
+ * DriveLogController
+ *
+ * @description :: Server-side logic for managing Drivelogs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/DriveLog.js
@@ -0,0 +1,27 @@
+/**
+ * DriveLog.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    lat: {
+      type: 'float'
+    },
+    lng: {
+      type: 'float'
+    },
+    battery: {
+      type: 'float'
+    },
+
+    belongs: {
+      model: "Vehicle"
+    }
+
+  }
+};
+
