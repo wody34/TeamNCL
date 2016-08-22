8bb06bd02068f91350380e544a7d19984f7f106c
Dongsik Yoon <dongsik.yoon@kaist.ac.kr>
2016-08-22 10:55:52 +0900

    현차에서 제공하는 충전소 반영

---
+++ api/controllers/EvPosController.js
@@ -0,0 +1,11 @@
+/**
+ * EvPosController
+ *
+ * @description :: Server-side logic for managing evpos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
